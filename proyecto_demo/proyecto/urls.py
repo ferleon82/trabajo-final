ab8b1b665a1e91fbba2cd54420caa8e4fc9e98d9
Luis Fernando León Pinzón <112999994+ferleon82@users.noreply.github.com>
2022-11-19 08:07:23 -0500

    Add files via upload

---
+++ proyecto_demo/proyecto/urls.py
@@ -0,0 +1,36 @@
+"""proyecto URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from app_productos.views import *
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('app_productos.urls')),
+    path('login/',login_view, name="vista_login"),
+    path('inicio/',inicio_view, name='vista_inicio'),
+    path('logout/', logout_view, name='vista_logout'),
+    path('nuevo/',nuevo_view, name="vista_nuevo"),
+    path('editar/<id>/', editar_view, name='vista_editar'),
+    path('eliminar/<id>/', eliminar_view, name='vista_eliminar'),
+    path('editar_stock/<id>/', editar_stock_view, name='vista_editar_stock'),
+    path('stock/<id>/',stock_view, name='vista_stock'),
+
+
+]
+
+admin.site.site_header = 'SISTEMA DE GESTION DE PRODUCTOS'
